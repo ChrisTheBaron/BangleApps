d940b533b585cbc35eaddb79288740a075e89acf
Chris Taylor <christhebaron@gmail.com>
2026-03-04 15:17:20 +0000

    Add recording logic

--- apps/rockclimb/app.js
+++ apps/rockclimb/app.js
@@ -1,3 +1,5 @@
+const appid = "rockclimb";
+
 const grades = [
     '4', '4+',
     '5', '5+',
@@ -22,18 +24,89 @@ const types = [
     'Weighted'
 ];
 
+const sampleRate = 1000;//TODO: Setting
+
 let typeIndex = 0;
 let inclineIndex = 0;
 let gradeIndex = 0;
 
+function getDateStr(d){
+    return d.toISOString().substring(0,10);
+}
+
+const today = getDateStr(new Date());
+
+function getTodayCount(){
+    const r = new RegExp("^" + appid + "\." + today + "\..*" + "\.json$");
+    const t = require("Storage").list(r);
+    return t.length;
+}
+
+// Recording state
+let recordData = [];
+let recordInterval;
+let hrValue = 0;
+let hrListener;
+let altValue = 0;
+let altListener;
+let startTime;
+
+function startRecording() {
+    recordData = [];
+    hrValue = 0;
+    altValue = 0;
+
+    Bangle.setHRMPower(true, appid);
+    hrListener = hr => { hrValue = hr.bpm; };
+    Bangle.on('HR', hrListener);
+
+    Bangle.setBarometerPower(true, appid);
+    altListener = alt => { altValue = alt.altitude; };
+    Bangle.on('pressure', altListener);
+
+    startTime = Date.now();
+
+    recordInterval = setInterval(() => {
+        const now = Date.now();
+        recordData.push({
+            t: now - startTime,
+            hr: hrValue,
+            alt: altValue
+        });
+    }, sampleRate);
+
+    E.showMenu({
+        "": { title:"Recording..." },
+        "Stop": stopRecording
+    });
+}
+
+function stopRecording() {
+    if(recordInterval) clearInterval(recordInterval);
+
+    if(hrListener) Bangle.removeListener('HR', hrListener);
+    Bangle.setHRMPower(false, appid);
+
+    if(altListener) Bangle.removeListener('pressure', altListener);
+    Bangle.setBarometerPower(false, appid);
+
+    const filename = appid+"."+getDateStr(new Date(startTime))+"."+(getTodayCount()+1)+".json";
+
+    require("Storage").writeJSON(filename, {
+        start: startTime,
+        type: types[typeIndex],
+        incline: inclines[inclineIndex],
+        grade: grades[gradeIndex],
+        data: recordData
+    });
+
+    showMainMenu();
+}
+
 function showMainMenu() {
     E.showMenu({
-        "": { title: "Rock Climb" },
-
-        "Record": () => {
-            // Your save logic here
-        },
-
+        "": { title: "Climb ("+getTodayCount()+" Today)" },
+        "Record": () => startRecording(),
         "Grade": {
             value: gradeIndex,
             min: 0,
@@ -43,7 +116,6 @@ function showMainMenu() {
             format: v => grades[v],
             onchange: v => gradeIndex = v
         },
-
         "Incline": {
             value: inclineIndex,
             min: 0,
@@ -53,7 +125,6 @@ function showMainMenu() {
             format: v => inclines[v],
             onchange: v => inclineIndex = v
         },
-
         "Type": {
             value: typeIndex,
             min: 0,
@@ -63,7 +134,6 @@ function showMainMenu() {
             format: v => types[v],
             onchange: v => typeIndex = v
         },
-
     });
 }
 
