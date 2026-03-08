4cd0e779aa09381f00c9f7092f8f9b54688565e8
Chris Taylor <christhebaron@gmail.com>
2026-03-08 00:06:08 +0000

    Show elapsed values

--- apps/rockclimb/app.js
+++ apps/rockclimb/app.js
@@ -32,7 +32,6 @@ let typeIndex = 0;
 let inclineIndex = 0;
 let gradeIndex = 0;
 
-
 function Holt(alpha, beta) {
     this.a = alpha;   // level smoothing
     this.b = beta;    // trend smoothing
@@ -49,13 +48,12 @@ Holt.prototype.add = function(x) {
         return x;
     }
 
-    var prev = this.l;
+    let prev = this.l;
     this.l = this.a * x + (1 - this.a) * (this.l + this.t);
     this.t = this.b * (this.l - prev) + (1 - this.b) * this.t;
 
     return this.l;
 };
-
 
 function getDateStr(d){
     return d.toISOString().substring(0,10);
@@ -69,31 +67,32 @@ function getTodayCount(){
     return t.length;
 }
 
+function elapsedString(ms) {
+    const s = (ms / 1000) | 0;
+    return `${((s/60)|0).toString().padStart(2, "0")}:${(s%60).toString().padStart(2,"0")}`;
+}
+
 // Recording state
 let recordInterval;
-let hrValue = 0;
 let hrListener;
-let altValue = 0;
 let altListener;
 let startTime;
 let fileName;
 let altSmoother;
 
-
 function startRecording() {
-    recordData = [];
-    hrValue = 0;
-    altValue = 0;
+    let hrValue = 0;
+    let altValue = 0;
     altSmoother = new Holt(0.4, 0.1);
     startTime = Date.now();
     fileName = appid+"."+getDateStr(new Date(startTime))+"."+(getTodayCount()+1);
-    dataFile = storage.open(fileName+".csv", 'a');
+    const dataFile = storage.open(fileName+".csv", 'a');
 
-    Bangle.setHRMPower(1);
+    Bangle.setHRMPower(true, appid);
     hrListener = hr => { hrValue = hr.bpm; };
     Bangle.on('HRM', hrListener);
 
-    Bangle.setBarometerPower(1);
+    Bangle.setBarometerPower(true, appid);
     altListener = alt => {
         altValue = altSmoother.add(alt.altitude);
     };
@@ -103,11 +102,21 @@ function startRecording() {
 
     recordInterval = setInterval(() => {
         dataFile.write(""+(Date.now() - startTime).toFixed(0)+","+hrValue+","+altValue.toFixed(2)+"\n");
+        showRecording(hrValue, altValue);
     }, sampleRate);
 
+    showRecording(hrValue, altValue);
+
+}
+
+function showRecording(hr, alt){
+    const elapsed = elapsedString(Date.now() - startTime);
     E.showMenu({
         "": { title:"Recording..." },
-        "Stop": stopRecording
+        "Stop": stopRecording,
+        "Elapsed": { value: elapsed },
+        "BPM": { value: hr.toFixed(0) },
+        "Alt": { value : alt.toFixed(0) }
     });
 }
 
@@ -115,10 +124,10 @@ function stopRecording() {
     if(recordInterval) clearInterval(recordInterval);
 
     if(hrListener) Bangle.removeListener('HRM', hrListener);
-    Bangle.setHRMPower(0);
+    Bangle.setHRMPower(false, appid);
 
     if(altListener) Bangle.removeListener('pressure', altListener);
-    Bangle.setBarometerPower(0);
+    Bangle.setBarometerPower(false, appid);
 
     storage.writeJSON(fileName + ".json", {
         start: startTime.toFixed(0),
