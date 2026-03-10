52bb354914510c087085d714349aa46dd0bcb274
Chris Taylor <christhebaron@gmail.com>
2026-03-10 13:51:42 +0000

    Update csv to include all raw data

--- apps/rockclimb/app.js
+++ apps/rockclimb/app.js
@@ -32,29 +32,6 @@ let typeIndex = 0;
 let inclineIndex = 0;
 let gradeIndex = 0;
 
-function Holt(alpha, beta) {
-    this.a = alpha;   // level smoothing
-    this.b = beta;    // trend smoothing
-    this.l = 0;       // level
-    this.t = 0;       // trend
-    this.init = false;
-}
-
-Holt.prototype.add = function(x) {
-    if (!this.init) {
-        this.l = x;
-        this.t = 0;
-        this.init = true;
-        return x;
-    }
-
-    let prev = this.l;
-    this.l = this.a * x + (1 - this.a) * (this.l + this.t);
-    this.t = this.b * (this.l - prev) + (1 - this.b) * this.t;
-
-    return this.l;
-};
-
 function getDateStr(d){
     return d.toISOString().substring(0,10);
 }
@@ -72,56 +49,41 @@ function elapsedString(ms) {
     return `${((s/60)|0).toString().padStart(2, "0")}:${(s%60).toString().padStart(2,"0")}`;
 }
 
-// Recording state
-let recordInterval;
 let hrListener;
 let altListener;
 let startTime;
 let fileName;
-let altSmoother;
 
 function startRecording() {
-    let hrValue = 0;
-    let altValue = 0;
-    altSmoother = new Holt(0.4, 0.1);
+    const count = getTodayCount()+1;
     startTime = Date.now();
-    fileName = appid+"."+getDateStr(new Date(startTime))+"."+(getTodayCount()+1);
+    fileName = appid+"."+getDateStr(new Date(startTime))+"."+count;
     const dataFile = storage.open(fileName+".csv", 'a');
+    dataFile.write("time,type,value\n");
 
     Bangle.setHRMPower(true, appid);
-    hrListener = hr => { hrValue = hr.bpm; };
+    hrListener = hr => {
+        if(hr.bpm > 0) dataFile.write(""+(Date.now() - startTime).toFixed(0)+",h,"+hr.bpm+"\n");
+    };
     Bangle.on('HRM', hrListener);
 
     Bangle.setBarometerPower(true, appid);
     altListener = alt => {
-        altValue = altSmoother.add(alt.altitude);
+        dataFile.write(""+(Date.now() - startTime).toFixed(0)+",a,"+alt.altitude.toFixed(2)+"\n");
     };
     Bangle.on('pressure', altListener);
 
-    dataFile.write("time,hr,alt\n");
-
-    recordInterval = setInterval(() => {
-        dataFile.write(""+(Date.now() - startTime).toFixed(0)+","+hrValue+","+altValue.toFixed(2)+"\n");
-        showRecording(hrValue, altValue);
-    }, sampleRate);
-
-    showRecording(hrValue, altValue);
-
-}
-
-function showRecording(hr, alt){
-    const elapsed = elapsedString(Date.now() - startTime);
     E.showMenu({
-        "": { title:"Recording..." },
-        "Stop": stopRecording,
-        "Elapsed": { value: elapsed },
-        "BPM": { value: hr.toFixed(0) },
-        "Alt": { value : alt.toFixed(0) }
+        "": { title:"Recording (" + count + ")"},
+        "Another": ()=> stopRecording("another"),
+        "Topped": () => stopRecording("topped"),
+        "Nearly": () => stopRecording("nearly"),
+        "Cancel": () => stopRecording("cancel"),
     });
+
 }
 
-function stopRecording() {
-    if(recordInterval) clearInterval(recordInterval);
+function stopRecording(state) {
 
     if(hrListener) Bangle.removeListener('HRM', hrListener);
     Bangle.setHRMPower(false, appid);
@@ -129,14 +91,26 @@ function stopRecording() {
     if(altListener) Bangle.removeListener('pressure', altListener);
     Bangle.setBarometerPower(false, appid);
 
-    storage.writeJSON(fileName + ".json", {
-        start: startTime.toFixed(0),
-        type: types[typeIndex],
-        incline: inclines[inclineIndex],
-        grade: grades[gradeIndex]
-    });
+    if(state !== "cancel"){
+        storage.writeJSON(fileName + ".json", {
+            start: startTime.toFixed(0),
+            end: Date.now().toFixed(0),
+            completed: (state === "topped" || state === "another"),
+            type: types[typeIndex],
+            incline: inclines[inclineIndex],
+            grade: grades[gradeIndex]
+        });
+    } else {
+        const dataFile = storage.open(fileName+".csv", 'r');
+        dataFile.erase();
+    }
 
-    showMainMenu();
+    if(state === "another"){
+        startRecording();
+    } else {
+        showMainMenu();
+    }
+
 }
 
 function showMainMenu() {
