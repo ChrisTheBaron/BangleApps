dd01c7cefb96a6fa1e92a7c867999059b13bca14
Chris Taylor <christhebaron@gmail.com>
2026-04-04 23:22:30 +0100

    Parse elevation data

--- apps/rockclimb/interface.js
+++ apps/rockclimb/interface.js
@@ -16,6 +16,7 @@ async function onInit(){
     }
 
     let files = {};
+    let csvs = {};
     let html = ``;
 
     const sessions = Object.groupBy(allfiles, f => f.substring(10, 20));
@@ -34,9 +35,10 @@ async function onInit(){
         for(let file of sessionfiles){
             const json = await new Promise(resolve => Util.readStorageJSON(file, resolve));
             files[file] = json;
+            const tags = getTags(json);
             const date = new Date(parseInt(json.start));
             html += `<details filename="${file}">
-  <summary>${date.toLocaleTimeString()} - ${json.grade} ${json.incline} ${json.type}</summary>
+  <summary>${date.toLocaleTimeString()} - ${json.grade} ${json.incline} ${tags.map(t => `#${t}`).join(' ')}</summary>
   <a href="#" action="download" filename="${file}">Download Climb</a>&nbsp;|&nbsp;<a href="#" action="delete" filename="${file}">Delete Climb</a>
   <canvas id="chart-${file}">Loading...</canvas>
 </details>`;
@@ -49,38 +51,60 @@ async function onInit(){
 
     document.querySelectorAll(`details[filename]`).forEach(detail => {
         detail.addEventListener('toggle', async () => {
+            Util.showModal(`Loading...`);
+
             const filename = detail.getAttribute('filename');
-            const datafile = await new Promise(resolve => Util.readStorageFile(filename.replace('.json', '.csv'), resolve));
-            const { elevation, averaged, minIndex, maxIndex, maxValue, minValue, diff } = parseElevation(datafile);
+            const metadata = files[filename];
+
+            if(!csvs[filename]){
+                csvs[filename] = await new Promise(resolve => Util.readStorageFile(filename.replace('.json', '.csv'), resolve));
+            }
+            const { averaged, ascentStartIndex, ascentFinishIndex, descentStartIndex, descentFinishIndex, diff} = parseElevation(csvs[filename]);
 
             const ctx = document.getElementById(`chart-${filename}`);
             const data = {
                 datasets: [
                     {
-                        label: 'Filtered',
+                        label: 'Elevation',
                         data: averaged,
                         backgroundColor: 'green'
                     },
                     //{
-                    //    label: 'Start',
+                    //    label: 'Ascent Start',
                     //    data: [
-                    //        {x: averaged[minIndex].x, y: minValue},
-                    //        {x: averaged[minIndex].x, y: maxValue}
+                    //        {x: averaged[ascentStartIndex].x, y: averaged[ascentStartIndex].y},
+                    //        {x: averaged[ascentStartIndex].x, y: averaged[ascentFinishIndex].y}
                     //    ],
                     //    type: 'line',
                     //},
                     //{
-                    //    label: 'End',
+                    //    label: 'Ascent End',
                     //    data: [
-                    //        {x: averaged[maxIndex].x, y: minValue},
-                    //        {x: averaged[maxIndex].x, y: maxValue}
+                    //        {x: averaged[ascentFinishIndex].x, y: averaged[ascentStartIndex].y},
+                    //        {x: averaged[ascentFinishIndex].x, y: averaged[ascentFinishIndex].y}
+                    //    ],
+                    //    type: 'line',
+                    //},
+                    //{
+                    //    label: 'Descent Start',
+                    //    data: [
+                    //        {x: averaged[descentStartIndex].x, y: averaged[descentStartIndex].y},
+                    //        {x: averaged[descentStartIndex].x, y: averaged[descentFinishIndex].y}
+                    //    ],
+                    //    type: 'line',
+                    //},
+                    //{
+                    //    label: 'Descent End',
+                    //    data: [
+                    //        {x: averaged[descentFinishIndex].x, y: averaged[descentStartIndex].y},
+                    //        {x: averaged[descentFinishIndex].x, y: averaged[descentFinishIndex].y}
                     //    ],
                     //    type: 'line',
                     //},
                 ]
             };
             const config = {
-                type: 'scatter',
+                type: 'line',
                 data: data,
                 options: {
                     animation: false,
@@ -88,17 +112,22 @@ async function onInit(){
                         x: {
                             type: 'linear',
                             position: 'bottom',
-                            min: averaged[minIndex].x,
-                            max: averaged[maxIndex].x
+                            min: averaged[ascentStartIndex].x,
+                            max: metadata.climbDown ? averaged[descentFinishIndex].x : averaged[ascentFinishIndex].x,
+                            ticks: {
+                                callback: (value)=> elapsedString(value - averaged[ascentStartIndex].x)
+                            }
                         },
                         y: {
-                            min: minValue - diff * 10,
-                            max: maxValue + diff * 10
+                            min: (metadata.climbDown ? Math.min(averaged[ascentStartIndex].y, averaged[descentFinishIndex].y) : averaged[ascentStartIndex].y) - diff * 10,
+                            max: (metadata.climbDown ? Math.max(averaged[ascentFinishIndex].y, averaged[descentStartIndex].y) : averaged[ascentFinishIndex].y) + diff * 10,
+                            display: false
                         }
                     }
                 }
             };
             new Chart(ctx, config);
+            Util.hideModal();
         }, { once: true });
     });
 
@@ -194,7 +223,7 @@ async function onInit(){
     }
 
     function parseElevation(datafile) {
-        let elevation = [];
+        let raw = [];
         const datapoints = datafile.split('\n');
         datapoints.shift(); // header
         datapoints.pop();   // trailing newline
@@ -203,18 +232,18 @@ async function onInit(){
             if(type !== 'a'){
                 return;
             }
-            elevation.push({ x: parseInt(offset), y: parseFloat(value) });
+            raw.push({ x: parseInt(offset), y: parseFloat(value) });
         });
 
         const first = lowPass(5);
         const second = lowPass(5);
         const third = lowPass(10);
 
-        const start = elevation[0].x;
-        const end = elevation[elevation.length-1].x;
+        const start = raw[0].x;
+        const end = raw[raw.length-1].x;
 
-        const forward = elevation.map(e => ({ x : e.x, y : first(e.x, e.y) }));
-        const backward = elevation.reverse().map(e => ({ x : e.x, y : second(end - e.x, e.y) })).reverse();
+        const forward = raw.map(e => ({ x : e.x, y : first(e.x, e.y) }));
+        const backward = raw.toReversed().map(e => ({ x : e.x, y : second(end - e.x, e.y) })).toReversed();
 
         const averaged = [];
 
@@ -224,46 +253,69 @@ async function onInit(){
             averaged.push({x: forward[i].x, y: third(forward[i].x, forward[i].y * f + backward[i].y * b)});
         }
 
-        const maxValue = Math.max(...averaged.map(x => x.y));
-        const minValue = Math.min(...averaged.map(x => x.y));
+        const peakElevation = Math.max(...averaged.map(x => x.y));
+        const lowestElevation = Math.min(...averaged.map(x => x.y));
 
-        const diff = (maxValue - minValue) * 0.01;
+        const diff = (peakElevation - lowestElevation) * 0.01;
 
-        const maxIndex = averaged.findIndex(x => x.y > maxValue - diff);
-        const minIndex = averaged.slice(0, maxIndex).findLastIndex(x => x.y < minValue + diff);
+        const ascentFinishIndex = averaged.findIndex(x => x.y > peakElevation - diff);
+
+        const ascentStartElevation = Math.min(...averaged.slice(0, ascentFinishIndex).map(x => x.y));
+        const ascentStartIndex = averaged.slice(0, ascentFinishIndex).findLastIndex(x => x.y < ascentStartElevation + diff);
+
+        const secondHalf = averaged.slice(ascentFinishIndex);
+        const descentFinishElevation = Math.min(...secondHalf.map(x => x.y));
+
+        const descentStartIndex = secondHalf.findLastIndex(x => x.y > peakElevation - diff) + ascentFinishIndex;
+        const descentFinishIndex = secondHalf.findIndex(x => x.y < descentFinishElevation + diff) + ascentFinishIndex;
 
         return {
-            elevation,
+            raw,
             averaged,
-            minValue,
-            maxValue,
-            minIndex,
-            maxIndex,
+            ascentStartIndex,
+            ascentFinishIndex,
+            descentStartIndex,
+            descentFinishIndex,
             diff
         };
     }
 
     /**
      * @param {String} filename
-     * @param {String} metadata
+     * @param {Object} metadata
      * @param {String[]} trkpts
+     * @param {Number} trackNo
      * @returns {string}
      */
-    function buildTrk(filename, metadata, trkpts) {
+    function buildTrk(filename, metadata, trkpts, trackNo) {
+        const { averaged, ascentStartIndex, ascentFinishIndex, descentStartIndex, descentFinishIndex } = parseElevation(csvs[filename]);
+        const tags = getTags(metadata);
+        let description = `Ascent Duration: ${elapsedString(averaged[ascentFinishIndex].x - averaged[ascentStartIndex].x)}`;
+        let timestamps = `ascent-start="${new Date(parseInt(metadata.start) + averaged[ascentStartIndex].x).toISOString()}"
+            ascent-finish="${new Date(parseInt(metadata.start) + averaged[ascentFinishIndex].x).toISOString()}"`;
+        if(metadata.climbDown){
+            timestamps += ` descent-start="${new Date(parseInt(metadata.start) + averaged[descentStartIndex].x).toISOString()}"
+            descent-finish="${new Date(parseInt(metadata.start) + averaged[descentFinishIndex].x).toISOString()}"`;
+            description += `\nDescent Duration: ${elapsedString(averaged[descentFinishIndex].x - averaged[descentStartIndex].x)}`;
+        }
+        if(tags.length > 0){
+            description += `\n${tags.map(t => `#${t}`).join(' ')}`;
+        }
         return `
 <trk>
   <metadata>
     <time>${new Date(parseInt(metadata.start)).toISOString()}</time>
     <extensions>
-      <climb:route incline="${metadata.incline}" grade="${metadata.grade}" completed="${metadata.completed ?? true}">
-        ${metadata.type !== 'Normal' ? `<climb:tag>${metadata.type}</climb:tag>` : ''}
-        ${metadata.weighted      ? `<climb:tag>Weighted</climb:tag>`    : ''}
-        ${metadata.autoBelay     ? `<climb:tag>Auto Belay</climb:tag>`  : ''}
-        ${metadata.climbDown     ? `<climb:tag>Climb Down</climb:tag>`  : ''}
+      <climb:route incline="${metadata.incline}" grade="${metadata.grade}" completed="${metadata.completed ?? true}" ${timestamps}>
+        ${tags.map(t => `<climb:tag>${t}</climb:tag>`).join('')}
       </climb:route>
     </extensions>
   </metadata>
-  <name>${filename.replace('.json', '')}</name>
+  <name>${metadata.grade} ${metadata.incline}</name>
+  <src>Bangle.js 2</src>
+  <number>${trackNo}</number>
+  <type>Indoor Climb</type>
+  <desc><![CDATA[${description}]]></desc>
   <trkseg>${trkpts.join('')}</trkseg>
 </trk>`;
     }
@@ -282,6 +334,7 @@ xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
 xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3"
 xmlns:climb="https://christhebaron.co.uk/BangleApps/apps/rockclimb/rockclimb.xsd">
 <metadata>
+  <time>${new Date().toISOString()}</time>
   <author>
     <name>Bangle.js</name>
     <link href="https://banglejs.com/apps/?id=rockclimb" />
@@ -295,12 +348,13 @@ ${tracks.join('\n')}
      * @param {String} filename
      * @returns {Promise<string|null>}
      */
-    async function buildTrkFromFile(filename) {
+    async function buildTrkFromFile(filename, trackNo) {
         const metadata = files[filename];
-        const datafile = await new Promise(resolve => Util.readStorageFile(filename.replace('.json', '.csv'), resolve));
-        if(!datafile) return null;
-        const trkpts = buildTrkpts(datafile, metadata.start);
-        return buildTrk(filename, metadata, trkpts);
+        if(!csvs[filename]){
+            csvs[filename] = await new Promise(resolve => Util.readStorageFile(filename.replace('.json', '.csv'), resolve));
+        }
+        const trkpts = buildTrkpts(csvs[filename], metadata.start);
+        return buildTrk(filename, metadata, trkpts, trackNo);
     }
 
     /**
@@ -310,9 +364,10 @@ ${tracks.join('\n')}
      */
     async function downloadSession(session, sessionfiles) {
         const tracks = [];
+        let trackNo = 1;
         for(const filename of sessionfiles) {
             Util.showModal(`Downloading (${tracks.length}/${sessionfiles.length})...`);
-            const trk = await buildTrkFromFile(filename);
+            const trk = await buildTrkFromFile(filename, trackNo++);
             if(!trk) continue;
             tracks.push(trk);
         }
@@ -324,7 +379,7 @@ ${tracks.join('\n')}
      * @returns {Promise<void>}
      */
     async function downloadData(filename) {
-        const trk = await buildTrkFromFile(filename);
+        const trk = await buildTrkFromFile(filename, 1);
         if(!trk) return;
         Util.saveFile(filename.replace('.json', '.gpx'), "gpx/xml", wrapGpx([trk]));
     }
@@ -336,6 +391,28 @@ ${tracks.join('\n')}
     async function deleteData(filename) {
         await new Promise(resolve => Util.eraseStorage(filename, resolve));
         await new Promise(resolve => Util.eraseStorageFile(filename.replace('.json', '.csv'), resolve));
+    }
+
+    function elapsedString(ms) {
+        const s = (ms / 1000) | 0;
+        return `${((s/60)|0).toString().padStart(2, "0")}:${(s%60).toString().padStart(2,"0")}`;
+    }
+
+    function getTags(metadata){
+        const tags = [];
+        if(metadata.type !== 'Normal'){
+            tags.push(metadata.type);
+        }
+        if(metadata.weighted){
+            tags.push("Weighted");
+        }
+        if(metadata.autoBelay){
+            tags.push("Auto Belay");
+        }
+        if(metadata.climbDown){
+            tags.push("Climb Down");
+        }
+        return tags;
     }
 
 }
